47eb6d3d6efcdbd04131a201bd72e16d44eac95b
Xil <mail@xil.boats>
2023-04-06 11:28:00 +0200

    create script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+stacks=$(curl -s --request GET \
+  --url https://$PORTAINER_URL/api/stacks \
+  --header "X-api-key: $PORTAINER_API_KEY")
+# Get all stack names
+ids=( $(echo "$stacks" | jq -r '.[] | .Id') )
+names=( $(echo "$stacks" | jq -r '.[] | .Name') )
+mkdir -p ./stacks
+for index in "${!ids[@]}"; do
+    echo "# ${names[$index]}" > ./stacks/"${names[$index]}".md
+    echo "\`\`\`yaml" >> ./stacks/"${names[$index]}".md
+    curl -s --request GET \
+  --url https://$PORTAINER_URL/api/stacks/${ids[$index]}/file \
+  --header "x-api-key: $PORTAINER_API_KEY" | jq -r '.StackFileContent' >> ./stacks/"${names[$index]}".md
+  echo "\`\`\`" >> ./stacks/"${names[$index]}".md
+done
