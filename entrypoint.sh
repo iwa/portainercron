0d5007e39e8d8e4a450c31ead3f74a0e1f9b9ada
Xil <mail@xil.boats>
2023-04-13 10:53:26 +0200

    try#10

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,5 @@ touch /etc/cron.d/portainercron
 echo "$CRON /app/script.sh >> /app/log 2>&1" > /etc/cron.d/portainercron
 chmod 0644 /etc/cron.d/portainercron
 crontab /etc/cron.d/portainercron
-su appuser
 echo "CA MARCHE POGGERS"
 cron -f
